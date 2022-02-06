f174e967af73a9da5f788933f0af417d8bacb430
cannelier2000 <cannelier@gmail.com>
2022-02-06 09:49:19 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  
+  strValue = "";
+
+  log = [];
+
+  genericEvent(event: any, name: string) {
+    this.trace(name + ':' + event.constructor.name);
+    console.log(event.constructor.name);
+  }
+
+  trace(str: string) {
+    this.log.push(str);
+    if (this.log.length > 10) {
+      console.log("shift");
+      this.log.shift();
+    }      
+  }
 }
